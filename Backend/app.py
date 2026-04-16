e63ecfd4d2c6c69616612b0321c1310cc41a9c00
Jash <jashpatel0210@gmail.com>
2026-04-17 02:00:29 +0530

    minor changes in formatting sales

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.prediction_routes import router as prediction_router
 from routes.product_routes import router as product_router
+from routes.sales_routes import router as sales_router
 
 app = FastAPI(title = "Product Management Analysis")
 
@@ -11,3 +12,4 @@ def home():
 
 app.include_router(prediction_router, prefix="/api/predictions", tags=["predictions"])
 app.include_router(product_router, prefix="/api/products", tags=["products"])
+app.include_router(sales_router , prefix="/api/sales", tags=["sales"])
--- Backend/models/sales_model.py
+++ Backend/models/sales_model.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import date
 
 class SalesData(BaseModel):
-    id : int
-    product_id : int
-    quantity : int
-    sale_date : date
-    discount  : float
+    id: int
+    product_id: int
+    quantity: int = Field(gt=0)
+    sale_date: date
+    discount: float = Field(default=0, ge=0, le=100)
