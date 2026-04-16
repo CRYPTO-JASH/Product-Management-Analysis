6e20df1481c9d77072a25584f07489420ea42acf
Jash <jashpatel0210@gmail.com>
2026-04-17 02:17:07 +0530

    implemented sales API with CSv upload and data validation

--- Backend/routes/sales_routes.py
+++ Backend/routes/sales_routes.py
@@ -1,14 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
+import pandas as pd
+import io
 from services.sales_service import add_sale, get_sales
 from models.sales_model import SalesData
 
 router = APIRouter()
 
 @router.post("/")
-def create_sale(sale: SalesData):
-    return add_sale(sale)
+def upload_sales(file: UploadFile = File(...)):
+    
+    contents = file.file.read()
+    df = pd.read_csv(io.StringIO(contents.decode("utf-8")))
+
+    data = df.to_dict(orient="records")
+
+    for row in data:
+        sale = SalesData(**row)   # convert dict → model
+        add_sale(sale)
+
+    return {
+        "message": f"Successfully uploaded {len(data)} sales records.",
+        "rows": len(data),
+        "preview": data[:5]
+    }
+
 
 @router.get("/")
 def fetch_sales():
     return get_sales()
-
